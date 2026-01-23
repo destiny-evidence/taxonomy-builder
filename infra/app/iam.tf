082cfb3c4c858510012b12e497a2e42c9326e899
Andrew Harvey <andrew@futureevidence.org>
2026-01-23 11:58:19 +1100

    Remove duplicated role assignment

--- infra/app/iam.tf
+++ infra/app/iam.tf
@@ -1,13 +1,3 @@
 # Role assignments for managed identity access
 
 # API Container App - ACR Pull
-resource "azurerm_role_assignment" "api_acr_pull" {
-  principal_id         = azurerm_user_assigned_identity.api.principal_id
-  scope                = data.azurerm_container_registry.this.id
-  role_definition_name = "AcrPull"
-
-  lifecycle {
-    ignore_changes = [principal_id, scope]
-  }
-}
-
