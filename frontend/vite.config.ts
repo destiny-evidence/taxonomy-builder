474d672599e2b15ccadb9d1a65d8da85831cfc7a
Andrew Harvey <andrew@futureevidence.org>
2026-02-01 22:40:05 +1100

    Config changes to get caddy hosting working

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,8 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    host: "0.0.0.0",
+    allowedHosts: [".localdev"],
     proxy: {
       "/api": {
         target: "http://localhost:8000",
