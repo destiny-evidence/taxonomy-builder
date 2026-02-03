0dd52a6dc146b4f28202a0881728689a4d654421
Andrew Harvey <andrew@futureevidence.org>
2026-02-03 14:44:14 +1100

    Fix display of namespace on projects

--- frontend/src/components/projects/ProjectList.tsx
+++ frontend/src/components/projects/ProjectList.tsx
@@ -57,14 +57,11 @@ export function ProjectList({ onEdit, onDeleted }: ProjectListProps) {
           >
             <div class="project-card__content">
               <h3 class="project-card__name">{project.name}</h3>
+              {project.namespace && (
+                <p class="project-card__namespace">{project.namespace}</p>
+              )}
               {project.description && (
                 <p class="project-card__description">{project.description}</p>
-              )}
-              {project.namespace && (
-                <p class="project-card__namespace">
-                  <span class="project-card__namespace-label">Namespace:</span>{" "}
-                  <code>{project.namespace}</code>
-                </p>
               )}
             </div>
             <div class="project-card__actions">
